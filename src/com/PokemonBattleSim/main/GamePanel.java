aa21d5fc037bce2d12ac27afcea7e8c37d307cf9
Eugenio Giampietri <168825934+realKrabZ@users.noreply.github.com>
2024-09-12 14:17:15 +0200

    New battle system and first test battle
    Updated the battle system (still WIP with lots of bugs and things to update) and created a first demo battle.

--- src/com/PokemonBattleSim/main/GamePanel.java
+++ src/com/PokemonBattleSim/main/GamePanel.java
@@ -3,8 +3,11 @@ package com.PokemonBattleSim.main;
 import java.awt.Color; // per colori
 import java.awt.Dimension; // per dimensione finestra
 import java.awt.Graphics;
+import java.awt.Image;
 //import java.awt.Graphics2D;
+import java.awt.Toolkit;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 
@@ -29,13 +32,52 @@ public class GamePanel extends JPanel implements Runnable{
 	private GameController gameController;
 	private Sound sound;
 	
+	// BATTAGLIA
+	private Trainer player;
+	private Trainer opponent;
+	private JButton atButton1;
+	private JButton atButton2;
+	private JButton atButton3;
+	private JButton atButton4;
+	private JButton swButton;
+	private Turn currentTurn = Turn.PLAYER_TURN;
+	
 	
 	public GamePanel() {
 		
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-		this.setBackground(Color.white);
 		this.setDoubleBuffered(true);
 		this.setFocusable(true);
+		
+		atButton1 = new JButton("Mossa 1");
+	    atButton2 = new JButton("Mossa 2");
+	    atButton3 = new JButton("Mossa 3");
+	    atButton4 = new JButton("Mossa 4");
+	    swButton = new JButton("Switch");
+
+	    atButton1.setActionCommand("MOVE1");
+	    atButton2.setActionCommand("MOVE2");
+	    atButton3.setActionCommand("MOVE3");
+	    atButton4.setActionCommand("MOVE4");
+	    swButton.setActionCommand("SWITCH");
+
+	    atButton1.addActionListener(e -> handleAction(e.getActionCommand()));
+	    atButton2.addActionListener(e -> handleAction(e.getActionCommand()));
+	    atButton3.addActionListener(e -> handleAction(e.getActionCommand()));
+	    atButton4.addActionListener(e -> handleAction(e.getActionCommand()));
+	    swButton.addActionListener(e -> handleSwitch());
+		
+/*		atButton1.setFocusable(false);
+		atButton2.setFocusable(false);
+		atButton3.setFocusable(false);
+		atButton4.setFocusable(false);
+		swButton.setFocusable(false); **/
+		
+		this.add(atButton1);
+	    this.add(atButton2);
+	    this.add(atButton3);
+	    this.add(atButton4);
+	    this.add(swButton);
 		
 		keyH = new KeyHandler(this);
 		this.addKeyListener(keyH);
@@ -45,6 +87,15 @@ public class GamePanel extends JPanel implements Runnable{
 		settings = new GameSettings(false);
 		gameController = new GameController(keyH);
 		sound = new Sound();
+		
+		PokemonPool pool = new PokemonPool();
+		
+		Pokemon pok1 = pool.genCharmander();
+		Pokemon pok2 = pool.genBulbasaur();
+		Pokemon pok3 = pool.genSquirtle();
+		
+		player = new Trainer("PietroSmusi", pok1, pok2, pok3);
+		opponent = new Trainer("Oksana", pok1, pok2, pok3);
 	}
 	
 	public void startGameThread() {
@@ -102,23 +153,101 @@ public class GamePanel extends JPanel implements Runnable{
 		
 		keyH.clearMouseClick();
 	}
+	
+	private enum Turn {
+	    PLAYER_TURN,
+	    OPPONENT_TURN
+	}
+	
+	private void handleAction(String actionCommand) {
+	    if (currentTurn == Turn.PLAYER_TURN) {
+	        Pokemon activePokemon = player.getActivePokemon();
+	        String moveName = "";
 
-	public void paintComponent(Graphics g) {
-//		DA IMPLEMENTARE
-		super.paintComponent(g);
-/*		
-		Graphics2D g2 = (Graphics2D)g; simile a Graphics ma con funzionalità in più
-		
-		TITLE SCREEN
-		if (gameState == titleState) {	
-		}
-		
-		else {
-		}
-		
- 		g2.dispose(); per risparmiare memoria */
+	        // Mappa i pulsanti alla mossa corretta
+	        switch (actionCommand) {
+	            case "MOVE1":
+	                moveName = "Growl"; // Modifica con la mossa corretta
+	                break;
+	            case "MOVE2":
+	                moveName = "Scratch";
+	                break;
+	            case "MOVE3":
+	                moveName = "Tackle";
+	                break;
+	            case "MOVE4":
+	                moveName = "Tail Whip";
+	                break;
+	        }
+
+	        // Imposta la mossa attiva
+	        if (!moveName.isEmpty()) {
+	            activePokemon.setActiveMove(moveName);
+	        }
+
+	        // Esegui la battaglia
+	        Battle battle = new Battle(player, opponent);
+	        battle.runBattle();
+
+	        // Cambia turno
+	        endTurn();
+	    }
 	}
 
+    private void handleSwitch() {
+        if (currentTurn == Turn.PLAYER_TURN) {
+        // Tasto per switchare pokémon?
+            endTurn();
+        }
+    }
+	
+	private void opponentTurn() {
+        // Azione della CPU?
+        endTurn();
+    }
+	
+	private void endTurn() {
+        if (currentTurn == Turn.PLAYER_TURN) {
+            currentTurn = Turn.OPPONENT_TURN;
+            opponentTurn();
+        } else {
+            currentTurn = Turn.PLAYER_TURN;
+        }
+    }
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Pokemon playerPokemon = player.getActivePokemon();
+	    Pokemon opponentPokemon = opponent.getActivePokemon();
+		
+	    Image playerSprite = Toolkit.getDefaultToolkit().getImage(getClass().getResource("sound/Robert.png"));
+	    Image opponentSprite = Toolkit.getDefaultToolkit().getImage(getClass().getResource("sound/Robert.png"));
+	    
+	    g.drawImage(playerSprite, 100, 400, this);
+	    g.drawImage(opponentSprite, 1117, 100, this);
+	    
+	    drawHealthBar(g, playerPokemon, 100, 370);
+	    drawHealthBar(g, opponentPokemon, 980, 70);
+	}
+
+	private void drawHealthBar(Graphics g, Pokemon pokemon, int x, int y) {
+	    // Lunghezza barra in base alla percentuale di HP rimasti
+	    int barWidth = 200;
+	    int hpWidth = (int)((pokemon.getHp() / pokemon.getMaxHp()) * barWidth);
+	    
+	    // Colore della barra (verde, giallo, rosso in base alla percentuale)
+	    Color barColor = Color.GREEN;
+	    if (pokemon.getHp() / pokemon.getMaxHp() < 0.5) barColor = Color.YELLOW;
+	    if (pokemon.getHp() / pokemon.getMaxHp() < 0.2) barColor = Color.RED;
+	    
+	    g.setColor(barColor);
+	    g.fillRect(x, y, hpWidth, 20); // Barra vita
+	    
+	    g.setColor(Color.BLACK);
+	    g.drawRect(x, y, barWidth, 20); // Contorno della barra
+	}
+	
 	public void playMusic(int i) {
 		sound.setFile(i);
 		sound.play();
