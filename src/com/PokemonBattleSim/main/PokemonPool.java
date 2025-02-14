c76dfbdfadce3512e7530685269f2c7a82b7840f
Eugenio Giampietri <168825934+realKrabZ@users.noreply.github.com>
2025-02-14 19:09:34 +0100

    Movesets updated.
    Final update to all of the movesets.

--- src/com/PokemonBattleSim/main/PokemonPool.java
+++ src/com/PokemonBattleSim/main/PokemonPool.java
@@ -26,27 +26,27 @@ public class PokemonPool {
 	}
 	
 	public static Pokemon genRobert() {
-		return new Pokemon("Robert", 48, PokemonType.NORMAL, PokemonMovePool.genGrowl(), PokemonMovePool.genTackle(), 5, 55, 50, 48, 50, 64);
+		return new Pokemon("Robert", 48, PokemonType.NORMAL, PokemonMovePool.genGrowl(), PokemonMovePool.genSlash(), 5, 55, 50, 48, 50, 64);
 	}
 	
 	public static Pokemon genUrlox() {
-		return new Pokemon("Urlox", 35, PokemonType.GRASS, PokemonMovePool.genTailWhip(), PokemonMovePool.genVineWhip(), 5, 50, 40, 70, 42, 70);
+		return new Pokemon("Urlox", 35, PokemonType.GRASS, PokemonMovePool.genLeer(), PokemonMovePool.genRazorLeaf(), 5, 50, 40, 70, 42, 70);
 	}
 	
 	public static Pokemon genCordol() {
-		return new Pokemon("Cordol", 42, PokemonType.NORMAL, PokemonMovePool.genGrowl(), PokemonMovePool.genTackle(), 5, 55, 60, 47, 55, 50);
+		return new Pokemon("Cordol", 42, PokemonType.NORMAL, PokemonMovePool.genGrowl(), PokemonMovePool.genBite(), 5, 55, 60, 47, 55, 50);
 	}
 	
 	public static Pokemon genSproloquio() {
-		return new Pokemon("Sproloquio", 45, PokemonType.WATER, PokemonMovePool.genTailWhip(), PokemonMovePool.genWaterGun(), 5, 52, 45, 61, 45, 80);
+		return new Pokemon("Sproloquio", 45, PokemonType.WATER, PokemonMovePool.genLeer(), PokemonMovePool.genHydroPump(), 5, 52, 45, 61, 45, 80);
 	}
 	
 	public static Pokemon genDomenico() {
-		return new Pokemon("Domenico", 43, PokemonType.NORMAL, PokemonMovePool.genGrowl(), PokemonMovePool.genTackle(), 5, 40, 73, 40, 75, 38);
+		return new Pokemon("Domenico", 43, PokemonType.NORMAL, PokemonMovePool.genGrowl(), PokemonMovePool.genSkullBash(), 5, 40, 73, 40, 75, 38);
 	}
 	
 	public static Pokemon genBentley() {
-		return new Pokemon("Bentley", 41, PokemonType.FIRE, PokemonMovePool.genTailWhip(), PokemonMovePool.genFlamethrower(), 5, 45, 69, 55, 67, 46);
+		return new Pokemon("Bentley", 41, PokemonType.FIRE, PokemonMovePool.genTailWhip(), PokemonMovePool.genFireSpin(), 5, 45, 69, 55, 67, 46);
 	}
 	
 	/**
